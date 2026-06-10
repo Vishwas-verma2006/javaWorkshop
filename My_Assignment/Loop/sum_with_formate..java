b3ac0ef20c854c94ede47a816aac12002bf398e6
Vishwas Verma <vishwasverma637@gmail.com>
2026-06-10 20:06:06 +0530

    Add code file to calculate sum with format

---
+++ My_Assignment/Loop/sum_with_formate..java
@@ -0,0 +1,25 @@
+public class Main
+{
+	
+	public static void main(String[] args) {
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		int a,b;
+		System.out.println("Enter the number");
+		a = sc.nextInt();
+		int sum=0;
+		
+        for(int i=1; i<=a; i++)
+        {   sum+=i;
+            char o = '+';
+            System.out.printf("%d",i);
+            if(i == a)
+            {
+                break;
+            }
+            System.out.printf("%c",o);
+        }
+        System.out.printf("="+sum);
+		
+	}
+}
+
